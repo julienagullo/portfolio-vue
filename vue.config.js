14494d8f0992734d3ef9f4429b54a22e1f28fd14
julienagullo <contact@avantage.digital>
2022-04-16 19:19:15 +0200

    Update the canonical and description in webpack

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ module.exports = {
   chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
+        args[0].canonical = 'https://jagullo.fr/'
         args[0].title = 'Développeur web sur Pau et Tarbes | jagullo.fr'
+        args[0].description = 'Bienvenue sur mon site Internet présentant mes travaux dans le domaine du web, du développement, du graphisme et de la communication sur Pau et Tarbes.'
         args[0].mode = process.env.NODE_ENV
         return args;
       })
